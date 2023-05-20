89072c55980c29510f718ec9a348697b561ba5f8
Karlo Vardijan <kvardijan20@foi.hr>
2023-05-20 18:13:47 +0200

    Kreirana klasa za generiranje tablice, kreiran konstruktor klase koji prima broj redova i stupaca.

---
+++ Transportium/GeneratorTablice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportium
+{
+    public class GeneratorTablice
+    {
+        private int _brojRedova;
+        private int _brojStupaca;
+        public GeneratorTablice(int brojRedova, int brojStupaca)
+        {
+            _brojRedova = brojRedova;
+            _brojStupaca = brojStupaca;
+        }
+    }
+}
