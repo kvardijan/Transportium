6e8b60db4eb6ebec94a00c2ed901e8781d4ef558
Karlo Vardijan <kvardijan20@foi.hr>
2023-05-21 12:01:02 +0200

    Napravljena klasa celije i klasa tablice transporta

---
+++ Transportium/Celija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportium
+{
+    public class Celija
+    {
+        public int TrosakPrijevoza { get; set; }
+        public int KolicinaTereta { get; set; }
+    }
+}
---
+++ Transportium/TablicaTransporta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportium
+{
+    public class TablicaTransporta
+    {
+        public List<int> KapacitetiIzvora { get; set; }
+        public List<int> PotrebeOdredista { get; set; }
+        public List<List<Celija>> TablicaCelija { get; set; }
+    }
+}
