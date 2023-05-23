f38cf2878e3efcf2eae98c375ddc8543ef73a3a7
Karlo Vardijan <kvardijan20@foi.hr>
2023-05-23 12:06:51 +0200

    Dodana metoda za popunjavanje liste slobodnih redova i stupaca kod minimalnih troskova. Implementirana metoda za pronalazenje relacije s najmanjim troskom prijevoza. Kreirana struktura koja pohranjuje podatke o relaciji najmanjeg troska.

--- Transportium/PocetniRaspored_MinimalniTroskovi.cs
+++ Transportium/PocetniRaspored_MinimalniTroskovi.cs
@@ -18,16 +18,88 @@ namespace Transportium
         {
             //ResetirajKolicine();
             DodajKolicine();
+            DodajSlobodneRedoveStupce();
             sumaKolicine = 0;
             int kolicina;
-            
+            PoljeNajmanjegTroska poljeNajmanjegTroska;
 
             do
             {
+                poljeNajmanjegTroska = OdrediPoljeNajmanjegTroska();
+                //vidi je li kolicina stupca ili reda manja i stavi tolko
+                //oduzmi od kolicine
+                //makni red ili stupac iz liste slobodnih
 
             } while (sumaKolicine < UpraviteljTablice.tablicaTransporta.SumaKolicine);
 
             return IzracunajPocetniZ();
+        }
+
+        private PoljeNajmanjegTroska OdrediPoljeNajmanjegTroska()
+        {
+            PoljeNajmanjegTroska poljeNajmanjegTroska = new PoljeNajmanjegTroska
+            {
+                trosak = int.MaxValue,
+                maxTeret = int.MinValue
+            };
+            int brojacR = 1;
+
+            for (int i = slobodniRedovi.First(); brojacR <= slobodniRedovi.Count; i = slobodniRedovi[brojacR])
+            {
+                int brojacS = 1;
+                for (int j = slobodniStupci.First(); brojacS <= slobodniStupci.Count; j = slobodniStupci[brojacS])
+                {
+                    Celija celija = UpraviteljTablice.tablicaTransporta.TablicaCelija[i][j];
+                    if (celija.TrosakPrijevoza < poljeNajmanjegTroska.trosak)
+                    {
+                        poljeNajmanjegTroska.trosak = celija.TrosakPrijevoza;
+                        poljeNajmanjegTroska.indexReda = i;
+                        poljeNajmanjegTroska.indexStupca = j;
+                        poljeNajmanjegTroska.maxTeret = IzracunajKolicinuTeretaZaStaviti(i, j);
+                    }
+                    else if (celija.TrosakPrijevoza == poljeNajmanjegTroska.trosak)
+                    {
+                        int maxTeretCelije = IzracunajKolicinuTeretaZaStaviti(i, j);
+                        if (maxTeretCelije > poljeNajmanjegTroska.maxTeret)
+                        {
+                            poljeNajmanjegTroska.trosak = celija.TrosakPrijevoza;
+                            poljeNajmanjegTroska.indexReda = i;
+                            poljeNajmanjegTroska.indexStupca = j;
+                            poljeNajmanjegTroska.maxTeret = maxTeretCelije;
+                        }
+                    }
+                    brojacS++;
+                }
+                brojacR++;
+            }
+
+            return poljeNajmanjegTroska;
+        }
+
+        private int IzracunajKolicinuTeretaZaStaviti(int indexReda, int indexStupca)
+        {
+            int kolicina;
+            if (kolicineIzvora[indexReda] > kolicineOdredista[indexStupca])
+            {
+                kolicina = kolicineOdredista[indexStupca];
+            }
+            else
+            {
+                kolicina = kolicineIzvora[indexReda];
+            }
+            return kolicina;
+        }
+
+        private void DodajSlobodneRedoveStupce()
+        {
+            for (int i = 1; i <= UpraviteljTablice.brojRedova; i++)
+            {
+                slobodniRedovi.Add(i);
+            }
+            for (int i = 1; i <= UpraviteljTablice.brojStupaca; i++)
+            {
+                slobodniStupci.Add(i);
+            }
         }
 
         private void DodajKolicine()
@@ -53,6 +125,14 @@ namespace Transportium
             string pocetniZ = "Z = ";
 
             return pocetniZ;
+        }
+
+        private struct PoljeNajmanjegTroska
+        {
+            public int trosak;
+            public int maxTeret;
+            public int indexReda;
+            public int indexStupca;
         }
     }
 }
