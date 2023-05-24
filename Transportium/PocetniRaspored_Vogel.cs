27c3a8cd584d7c0fdc48cb378570812ecf1347c2
Karlo Vardijan <kvardijan20@foi.hr>
2023-05-24 12:39:38 +0200

    Dodane metode DodajKolicine i DodajSlobodneRedoveStupce.

--- Transportium/PocetniRaspored_Vogel.cs
+++ Transportium/PocetniRaspored_Vogel.cs
@@ -8,5 +8,40 @@ namespace Transportium
 {
     public class PocetniRaspored_Vogel
     {
+        int[] kolicineIzvora = new int[11];
+        int[] kolicineOdredista = new int[11];
+        int sumaKolicine;
+        List<int> slobodniRedovi = new List<int>();
+        List<int> slobodniStupci = new List<int>();
+
+        public void RjesiRasporedivanje()
+        {
+            DodajKolicine();
+            DodajSlobodneRedoveStupce();
+        }
+
+        private void DodajKolicine()
+        {
+            for (int i = 1; i <= UpraviteljTablice.brojRedova; i++)
+            {
+                kolicineIzvora[i] = UpraviteljTablice.tablicaTransporta.KapacitetiIzvora[i];
+            }
+            for (int i = 1; i <= UpraviteljTablice.brojStupaca; i++)
+            {
+                kolicineOdredista[i] = UpraviteljTablice.tablicaTransporta.PotrebeOdredista[i];
+            }
+        }
+
+        private void DodajSlobodneRedoveStupce()
+        {
+            for (int i = 1; i <= UpraviteljTablice.brojRedova; i++)
+            {
+                slobodniRedovi.Add(i);
+            }
+            for (int i = 1; i <= UpraviteljTablice.brojStupaca; i++)
+            {
+                slobodniStupci.Add(i);
+            }
+        }
     }
 }
