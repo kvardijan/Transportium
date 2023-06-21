93eb6f8dd515bb6f0eee40322d1f25aa1d912a65
Karlo Vardijan <kvardijan20@foi.hr>
2023-06-21 18:21:48 +0200

    Implementirana metoda za provjeravanje koja relacija je odsjecena od drugih.

--- Transportium/Degeneracija.cs
+++ Transportium/Degeneracija.cs
@@ -10,7 +10,43 @@ namespace Transportium
     {
         public void RijesiDegeneraciju()
         {
+            //tu mozda provjeru ranga da znamo kolka je razlika da znamo kolko fiktivnih relacija napraviti
+            Celija degeneriranaRelacija = ProvjeriZavisnostiRelacija();
+            DohvatiPotencijanuRelacijuRjesenjaDegenaracije(degeneriranaRelacija);
+        }
 
+        private Celija ProvjeriZavisnostiRelacija()
+        {
+            Celija degeneriranaRelacija = null;
+            for (int i = 1; i <= UpraviteljTablice.brojRedova; i++)
+            {
+                for (int j = 1; j <= UpraviteljTablice.brojStupaca; j++)
+                {
+                    Celija celija = UpraviteljTablice.tablicaTransporta.TablicaCelija[i][j];
+                    if (celija.Zauzeto)
+                    {
+                        int brojPovezanihRelacija = IzbrojiZauzeteCelijeReda(i);
+                        brojPovezanihRelacija = IzbrojiZauzeteCelijeStupca(j);
+                        if (brojPovezanihRelacija == 1) degeneriranaRelacija = celija;
+                    }
+                }
+            }
+            return degeneriranaRelacija;
+        }
+
+        private void DohvatiPotencijanuRelacijuRjesenjaDegenaracije(Celija degeneriranaRelacija)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int IzbrojiZauzeteCelijeStupca(int indexStupca)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int IzbrojiZauzeteCelijeReda(int indexReda)
+        {
+            throw new NotImplementedException();
         }
     }
 }
